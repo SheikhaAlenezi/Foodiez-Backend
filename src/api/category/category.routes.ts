3df9b0df592e3eda883ba9c745e3ee0adf2bd57c
SheikhaAlenezi <shei5ha98@gmail.com>
2025-09-16 18:31:56 +0300

    updates

--- src/api/category/category.routes.ts
+++ src/api/category/category.routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { createList, getAllCategory } from "./category.controllers";
+import {
+  createList,
+  deleteCategory,
+  getAllCategory,
+  getRecipesByCategory,
+} from "./category.controllers";
 import { authenticate } from "../../middlewares/authenticate";
 
 const router = express.Router();
 
 router.post("/createList", authenticate, createList);
 router.get("/getAllCategory", getAllCategory);
-
+router.delete(":id", deleteCategory);
+router.get("/:id/recipes", getRecipesByCategory);
 export default router;
